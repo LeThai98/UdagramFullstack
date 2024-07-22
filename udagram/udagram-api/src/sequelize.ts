e8ec95149b581ee46a5002ea87c616405176f343
LeThai98 <hongthaifamily@gmail.com>
2024-07-22 22:35:52 +0700

    ssl encryption

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -9,4 +9,25 @@ export const sequelize = new Sequelize({
 
   dialect: "postgres",
   storage: ":memory:",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
+
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   dialect: "postgres",
+//   storage: ":memory:",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+// });
