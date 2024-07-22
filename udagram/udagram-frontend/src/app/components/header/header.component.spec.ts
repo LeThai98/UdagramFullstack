ae29cbee2f4bbd9af9ccd3ac96bd674691c8b16f
LeThai98 <hongthaifamily@gmail.com>
2024-07-22 13:53:42 +0700

    unit test

--- udagram/udagram-frontend/src/app/components/header/header.component.spec.ts
+++ udagram/udagram-frontend/src/app/components/header/header.component.spec.ts
@@ -1,24 +1,39 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { HeaderComponent } from './header.component';
-// import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core'; // Optionally import these to avoid schema errors
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeaderComponent } from './header.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core'; // Optionally import these to avoid schema errors
+import { AuthService } from '../../services/auth.service';
+import {  provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
-// describe('HeaderComponent', () => {
-//   let component: HeaderComponent;
-//   let fixture: ComponentFixture<HeaderComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [HeaderComponent], // Declare the component here
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA] // Optionally add these schemas
-//     })
-//     .compileComponents();
+  //declare mock
+  let authServiceMock;
 
-//     fixture = TestBed.createComponent(HeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj<AuthService>(['login','logout']);
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, BrowserDynamicTestingModule],
+      declarations: [HeaderComponent], // Declare the component here
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA], // Optionally add these schemas,
+      providers:[
+        provideHttpClientTesting(),
+        {provide:AuthService, useValue:  authServiceMock}
+      ]
+    })
+    .compileComponents().catch(e => console.log(e));
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  
+});
